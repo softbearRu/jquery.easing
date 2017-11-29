a55c997bd262ff963a3ce9e2f2cc30a4d39f6947
softbear <c@softbear.ru>
2017-11-29 11:41:52 +0300

    custom bounceOut

--- jquery.easing.js
+++ jquery.easing.js
@@ -40,8 +40,8 @@ function bounceOut(x) {
 		d1 = 2.75;
 	if ( x < 1/d1 ) {
 		return n1*x*x;
-	} else if ( x < 2/d1 ) {
-		return n1*(x-=(1.5/d1))*x + .75;
+	} else if ( x < 2.3/d1 ) {
+		return n1*(x-=(1.5/d1))*x + .90;
 	} else if ( x < 2.5/d1 ) {
 		return n1*(x-=(2.25/d1))*x + .9375;
 	} else {
